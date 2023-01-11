4b3c67a99c7afd9a348ee912c96302f34b41d293
sushant <yadavsushant343@gmail.com>
2023-01-11 14:55:54 +0530

    created main window

---
+++ app.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+
+window.geometry("500x350")
+window.maxsize(500, 350)
+window.minsize(500, 350)
+
+window.title("Website blocker")
+
+
+if __name__ == '__main__':
+    mainloop()
