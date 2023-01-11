dcd4c9a04ce97f43c9dbe8bb8d302ca4cacf20e1
sushant <yadavsushant343@gmail.com>
2023-01-11 19:40:20 +0530

    block functionality and show functionality added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from utils import *
 
 window = Tk()
 
@@ -8,6 +9,25 @@ window.minsize(500, 350)
 
 window.title("Website blocker")
 
+label1 = Label(window, text="Add website")
+label1.place(x=5,y=20)
+input1 = Text(window, font='Arial', height=1, width=40)
+input1.place(x=90,y=20)
+button1 = Button(window, text="Add", command=lambda: blockWebsite(input1.get(1.0, END)))
+button1.place(x=5,y=53)
+
+frame = LabelFrame(window, text="Blocked website list", padx=4, pady=4)
+frame.place(x=5,y=85)
+
+def showBlockedWebsites():
+    r = 0
+    websiteList = getBlockedWebsites()
+    for website in websiteList:
+        Label(frame, text=website).grid(row=r,column=0)
+        Button(frame, text='unblock').grid(row=r,column=1)
+        r = r + 1
+
+showBlockedWebsites()
 
 if __name__ == '__main__':
     mainloop()
---
+++ utils.py
@@ -0,0 +1,30 @@
+from tkinter import messagebox
+
+ip_address = '127.0.0.1'
+host_path = r'C:\Windows\System32\drivers\etc\hosts'
+
+def blockWebsite(website):
+    website = website.strip('\n')
+    if len(website) > 12:
+        messagebox.showwarning("warning", "Enter a valid website name")
+    website_path = ip_address + ' www.' + website + '.com\n'
+    with open(host_path, 'r+') as host_file:
+        in_block_list = False
+        for line in host_file:
+            if line == website_path:
+                in_block_list = True
+                break
+        if in_block_list is False:
+            host_file.write(website_path)
+            messagebox.showinfo("success", website + ' is blocked')
+        else:
+            messagebox.showinfo("", website + ' is already in block list')
+        
+
+def getBlockedWebsites():
+    list = []
+    with open(host_path, 'r') as host_file:
+        for line in host_file:
+            if len(line) > 10 and line[:9] == ip_address:
+                list.append(line[10:].strip('\n'))
+    return list
